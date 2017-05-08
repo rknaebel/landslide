55935cd92bcfab43ff008a0ed217aeb3d03ca220
Rene Knaebel <reneknaebel90@gmail.com>
2017-05-09 01:00:30 +0200

    hdf5 generator started working on evaluation

--- dataset.py
+++ dataset.py
@@ -7,12 +7,37 @@ import matplotlib.pyplot as plt
 
 import math
 
+###############################################################################
+###############################################################################
+import logging
+###############################################################################
+
+# create logger
+logger = logging.getLogger('logger')
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+###############################################################################
+###############################################################################
+
 # global params
 fld = 'data/'
 
 sateliteImages = ['20090526', '20110514', '20120524', '20130608',
                   '20140517', '20150507', '20160526']
-sateliteImages = sateliteImages[:3]
+sateliteImages = sateliteImages
 alt = 'DEM_altitude.tif'
 slp = 'DEM_slope.tif'
 
@@ -61,13 +86,14 @@ def loadLandslideDataset(dates):
 
 
 def getLandslideDataFor(date):
+    altitute, slope = loadStaticData()
     date_idx = sateliteImages.index(date)
     prev_date_idx = date_idx - 1 if date_idx >= 1 else 0
-    img, nvdi, mask = loadSateliteFile(date)
-    prev_img, prev_nvdi, prev_mask = loadSateliteFile(sateliteImages[prev_date_idx])
-    image = np.concatenate((img, np.expand_dims(nvdi, 2)), axis=2)
-    prev_image = np.concatenate((prev_img, np.expand_dims(prev_nvdi, 2)), axis=2)
-    image = np.concatenate((image, prev_image), axis=2)
+    img, ndvi, mask = loadSateliteFile(date)
+    prev_img, prev_ndvi, _ = loadSateliteFile(sateliteImages[prev_date_idx])
+    image = np.concatenate((img, np.expand_dims(ndvi, 2)), axis=2)
+    prev_image = np.concatenate((prev_img, np.expand_dims(prev_ndvi, 2)), axis=2)
+    image = np.concatenate((image, prev_image, np.expand_dims(altitute, 2), np.expand_dims(slope, axis=2)), axis=2)
     return image, mask
 
 
@@ -81,19 +107,143 @@ def patchValidator(shape, pos, size):
 
 
 def getDataset():
+    logger.info("load landslides and masks")
     sat_images, masks = zip(*(getLandslideDataFor(d) for d in sateliteImages))
-    sat_images = np.stack(sat_images)
-    altitute, slope = loadStaticData(normalize=True)
+    sat_images = np.stack(sat_images, axis=0)
 
+    logger.info("calculate coordinates per mask")
     positives, negatives = [], []
-    for idx, mask in enumerate(masks):
+    for year, mask in enumerate(masks):
+        logger.info("  process mask {}".format(year))
+        num_pos = int(mask.sum())
+        num_neg = int(mask.size - num_pos)
+
         pos = zip(*np.where(mask == 1))
-        positives += [(idx, x, y) for x, y in pos]
+        # positives.append(np.array([(idx, x, y) for x, y in pos]))
+        positive = np.empty((num_pos,3), dtype=np.float32)
+        for idx, (x, y) in enumerate(pos):
+            positive[idx] = (year, x, y)
+        positives.append(positive)
+
         neg = zip(*np.where(mask == 0))
-        negatives += [(idx, x, y) for x, y in neg]
+        # negatives.append(np.array([(year, x, y) for x, y in neg]))
+        negative = np.empty((num_neg, 3), dtype=np.float32)
+        for idx, (x, y) in enumerate(neg):
+            negative[idx] = (year, x, y)
+        negatives.append(negative)
+
+    logger.info("concatenate coordinates")
+    positives = np.concatenate(positives)
+    negatives = np.concatenate(negatives)
 
     return sat_images, positives, negatives, altitute, slope
 
+import h5py
+
+def makeH5Dataset(path):
+    f = h5py.File(path, "w")
+
+    logger.info("load landslides and masks")
+    sat_images, masks = zip(*(getLandslideDataFor(d) for d in sateliteImages))
+    sat_images = np.stack(sat_images, axis=0)
+
+    f.create_dataset("sat_images", data=sat_images)
+    del sat_images
+
+    logger.info("calculate coordinates per mask")
+    positives, negatives = [], []
+    for year, mask in enumerate(masks):
+        logger.info("  process mask {}".format(year))
+        num_pos = int(mask.sum())
+        num_neg = int(mask.size - num_pos)
+
+        pos = zip(*np.where(mask == 1))
+        # positives.append(np.array([(idx, x, y) for x, y in pos]))
+        positive = np.empty((num_pos,3), dtype=np.int32)
+        for idx, (x, y) in enumerate(pos):
+            positive[idx] = (year, x, y)
+        positives.append(positive)
+
+        neg = zip(*np.where(mask == 0))
+        # negatives.append(np.array([(year, x, y) for x, y in neg]))
+        negative = np.empty((num_neg, 3), dtype=np.int32)
+        for idx, (x, y) in enumerate(neg):
+            negative[idx] = (year, x, y)
+        negatives.append(negative)
+
+    logger.info("concatenate coordinates")
+    positives = np.concatenate(positives)
+    negatives = np.concatenate(negatives)
+
+    f.create_dataset("pos", data=positives)
+    f.create_dataset("neg", data=negatives)
+
+    return True
+
+def indexGenerator(data, validator, image_size, size, batch_size):
+    """
+    
+    :param data: fh on h5py dataset
+    :param validator: filter applied to every position
+    :param batch_size: 
+    :return: 
+    """
+    batch = np.empty((batch_size,3), dtype=np.int32)
+    ctr = 0
+    while True:
+        indices = np.random.permutation(len(data))
+        for i in indices:
+            if validator(image_size,data[i][1:],size):
+                batch[ctr] = data[i]
+                ctr += 1
+            if ctr == batch_size:
+                yield batch
+                ctr = 0
+
+
+def fullIndexGenerator(image_size, size, batch_size):
+    """
+
+    :param data: fh on h5py dataset
+    :param validator: filter applied to every position
+    :param batch_size: 
+    :return: 
+    """
+    batch = np.empty((batch_size, 3), dtype=np.int32)
+    ctr = 0
+    while True:
+        indices = np.random.permutation(len(data))
+        for i in indices:
+            if validator(image_size, data[i][1:], size):
+                batch[ctr] = data[i]
+                ctr += 1
+            if ctr == batch_size:
+                yield batch
+                ctr = 0
+
+
+def patchGeneratorFromH5(path, size=25, batch_size=64, p=0.4):
+    data = h5py.File(path, "r")
+    # calculate the batch size per label
+    batch_size_pos = int(batch_size * p)
+    batch_size_neg = batch_size - batch_size_pos
+    image_size = data["sat_images"].shape[1:]
+    # init index generators
+    idx_pos = indexGenerator(data["pos"], patchValidator, image_size, size, batch_size_pos)
+    idx_neg = indexGenerator(data["neg"], patchValidator, image_size, size, batch_size_neg)
+
+    for sample_idx_pos, sample_idx_neg in zip(idx_pos, idx_neg):
+        #
+        X = np.stack([
+            *map(lambda x: extractPatch(data["sat_images"], x[0], x[1], x[2], size), sample_idx_pos),
+            *map(lambda x: extractPatch(data["sat_images"], x[0], x[1], x[2], size), sample_idx_neg)
+        ])
+        #
+        y = np.concatenate((
+            np.ones( batch_size_pos, dtype=np.float32),
+            np.zeros(batch_size_neg, dtype=np.float32)
+        ))
+        yield X, y
 
 def LandslideGenerator(data, size=25, batch_size=64, p=0.2, normalize=True):
     sat_images, positives, negatives, altitute, slope = data
@@ -151,3 +301,10 @@ def LandslideData(date, size=25, normalize=True):
     ))
     return X, y
 
+
+if __name__ == "__main__":
+    path = "/tmp/landslide/data.h5"
+    makeH5Dataset(path)
+    gen = patchGeneratorFromH5(path, 25, 128, 0.4)
+    for i, (X,y) in enumerate(gen):
+        print(i, X.shape, y.shape)
--- main.py
+++ main.py
@@ -6,12 +6,13 @@ import networks
 
 from evaluation import precision, recall, f1_score
 
-SIZE = 15
-BATCH_SIZE = 64
-QUEUE_SIZE = 20
+SIZE = 25
+BATCH_SIZE = 256
+QUEUE_SIZE = 50
 EPOCHS = 10
+STEPS_PER_EPOCH = 100000 // BATCH_SIZE
 
-def main():
+def main2():
     print("load full data into memory")
     data = dataset.getDataset()
     print("use date inside patch generator")
@@ -47,6 +48,69 @@ def main():
     
     model.save("./model.h5")
 
+def main():
+    print("initialize patch generator")
+    lsg = dataset.patchGeneratorFromH5("/tmp/landslide/data.h5",
+                                       size=SIZE,
+                                       batch_size=BATCH_SIZE,
+                                       p=0.4)
+
+    model = Sequential()
+    model.add(Conv2D(32, (5, 5), input_shape=(SIZE, SIZE, 14)))
+    model.add(Activation('relu'))
+    model.add(Conv2D(16, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPool2D((1, 1), strides=(1, 1)))
+    model.add(Dropout(0.25))
+
+    # model.add(Dense(512, activation='relu', name='dense'))
+    # model.add(Dropout(0.25))
+
+    model.add(AvgPool2D((3, 3), strides=(1, 1)))
+    model.add(Flatten(name="flatten"))
+
+    model.add(Dense(1, name='last_layer'))
+    model.add(Activation('sigmoid'))
+
+    model.compile(optimizer="adam",
+                  loss="binary_crossentropy",
+                  metrics=["accuracy", precision, recall, f1_score])
+
+    model.fit_generator(lsg,
+                        steps_per_epoch=STEPS_PER_EPOCH,
+                        epochs=EPOCHS,
+                        verbose=True,
+                        max_q_size=QUEUE_SIZE,
+                        workers=1)
+
+    model.save("./model.h5")
+
+from keras.models import load_model
+import h5py
+import matplotlib.pyplot as plt
+
+def padding(x,p):
+    return np.lib.pad(x,((0,0),(p,p),(p,p),(0,0)),'constant', constant_values=(0,))
+
+def evaluate_model():
+    model = load_model("model.h5", custom_objects={"precision": precision,
+                                                   "recall": recall,
+                                                   "f1_score": f1_score})
+    data = h5py.File("/tmp/landslide/data.h5", "r")
+    img = data["sat_images"][-1:]
+    pred = np.empty(img.shape[1:-1])
+    img = padding(data["sat_images"][-1:], 12)
+
+    for x in range(pred.shape[0]):
+        patches = np.stack([dataset.extractPatch(img, 0, x+12, y+12, 25) for y in range(pred.shape[1])])
+        pred[x] = model.predict(patches).T
+        if x % 10:
+            plt.imshow(pred)
+            plt.show()
+        print(x)
+
 
 if __name__ == "__main__":
-    main()
+    #main()
+    #evaluate_model()
+    pass
