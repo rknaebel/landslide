515c9a127f901d1a41218f7545d4c1d415421fcd
Rene Knaebel <reneknaebel90@gmail.com>
2017-05-10 12:46:54 +0200

    integrated argparser for better handlng

--- main.py
+++ main.py
@@ -7,22 +7,46 @@ import networks
 
 from evaluation import precision, recall, f1_score
 
-TEMP_PATH = "tmp/"
+################################################################################
 
-SIZE = 25
-BATCH_SIZE = 128
-QUEUE_SIZE = 50
-EPOCHS = 10
-STEPS_PER_EPOCH = 100000 // BATCH_SIZE
+import argparse
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--data", action="store", dest="data",
+                    default="tmp/data.h5")
+
+parser.add_argument("--model", action="store", dest="model",
+                    default="models/model.h5")
+
+parser.add_argument("--batch", action="store", dest="batch_size",
+                    default=64, type=int)
+
+parser.add_argument("--epochs", action="store", dest="epochs",
+                    default=10, type=int)
+
+parser.add_argument("--samples", action="store", dest="samples",
+                    default=4, type=int)
+
+parser.add_argument("--area", action="store", dest="area_size",
+                    default=25, type=int)
+
+parser.add_argument("--queue", action="store", dest="queue_size",
+                    default=50, type=int)
+
+args = parser.parse_args()
+
+args.steps_per_epoch = args.samples // args.batch_size
+
+################################################################################
 
 def main2():
     print("load full data into memory")
     data = dataset.getDataset()
     print("use date inside patch generator")
-    lsg = dataset.LandslideGenerator(data, SIZE, BATCH_SIZE, 0.4, True)
+    lsg = dataset.LandslideGenerator(data, args.area_size, args.batch_size, 0.4, True)
 
     model = Sequential()
-    model.add(Conv2D(32, (5, 5), input_shape=(SIZE, SIZE, 12)))
+    model.add(Conv2D(32, (5, 5), input_shape=(args.area_size, args.area_size, 12)))
     model.add(Activation('relu'))
     model.add(Conv2D(16, (3, 3)))
     model.add(Activation('relu'))
@@ -43,28 +67,28 @@ def main2():
                   metrics=["accuracy", precision, recall, f1_score])
 
     model.fit_generator(lsg,
-                        steps_per_epoch=10000,
-                        epochs=EPOCHS,
+                        steps_per_epoch=args.steps_per_epoch,
+                        epochs=args.epochs,
                         verbose=True,
-                        max_q_size=QUEUE_SIZE,
+                        max_q_size=args.queue_size,
                         workers=1)
     
     model.save("./model.h5")
 
 def main():
     print("initialize patch generator")
-    train_gen = dataset.patchGeneratorFromH5(TEMP_PATH + "data.h5",
-                                             size=SIZE,
-                                             batch_size=BATCH_SIZE,
+    train_gen = dataset.patchGeneratorFromH5(args.data,
+                                             size=args.area_size,
+                                             batch_size=args.batch_size,
                                              p=0.2)
 
-    val_gen = dataset.patchGeneratorFromH5(TEMP_PATH + "data.h5",
-                                           size=SIZE,
-                                           batch_size=BATCH_SIZE,
+    val_gen = dataset.patchGeneratorFromH5(args.data,
+                                           size=args.area_size,
+                                           batch_size=args.batch_size,
                                            p=0.01)
 
     model = Sequential()
-    model.add(Conv2D(32, (5, 5), input_shape=(SIZE, SIZE, 14)))
+    model.add(Conv2D(32, (5, 5), input_shape=(args.area_size, args.area_size, 14)))
     model.add(Activation('relu'))
     model.add(Conv2D(16, (3, 3)))
     model.add(Activation('relu'))
@@ -88,15 +112,15 @@ def main():
 
 
     model.fit_generator(train_gen,
-                        steps_per_epoch=STEPS_PER_EPOCH,
-                        epochs=EPOCHS,
+                        steps_per_epoch=args.steps_per_epoch,
+                        epochs=args.epochs,
                         validation_data=val_gen,
                         validation_steps=100,
                         verbose=True,
-                        max_q_size=QUEUE_SIZE,
+                        max_q_size=args.queue_size,
                         workers=1)
 
-    model.save(TEMP_PATH + "model.h5")
+    model.save(args.model)
 
 from keras.models import load_model
 import evaluation
