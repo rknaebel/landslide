3958db0b15a3998481a9cb28814d6eeb946d0961
rknaebel <rknaebel@uni-potsdam.de>
2017-05-16 14:50:25 +0200

    * added cifar model implementation from keras website

--- networks.py
+++ networks.py
@@ -1,5 +1,6 @@
 from keras.layers import Activation, AvgPool2D, Conv2D, Dense, Dropout, Flatten, MaxPool2D
 from keras.models import Model, Sequential
+from keras.layers import Input, merge
 
 from utils import Maxout
 
@@ -74,30 +75,54 @@ def get_model_2(area):
     return model
 
 
-from keras.layers import Input, merge
+def get_model_cifar(area):
+    model = Sequential()
+
+    model.add(Conv2D(32, (3, 3), padding='same', input_shape=(area, area, 14)))
+    model.add(Activation('relu'))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPool2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+
+    return model
 
 
 def get_model_3(area):
     """First inception network implementation"""
     x = input_image = Input(shape=(area, area, 14))
 
-    tower_0 = Conv2D(64, 1, 1, border_mode='same', activation='relu')(x)
-    tower_1 = Conv2D(64, 1, 1, border_mode='same', activation='relu')(x)
-    tower_1 = Conv2D(64, 3, 3, border_mode='same', activation='relu')(tower_1)
-    tower_2 = Conv2D(64, 1, 1, border_mode='same', activation='relu')(x)
-    tower_2 = Conv2D(64, 5, 5, border_mode='same', activation='relu')(tower_2)
+    tower_0 = Conv2D(64, (1, 1), border_mode='same', activation='relu')(x)
+    tower_1 = Conv2D(64, (1, 1), border_mode='same', activation='relu')(x)
+    tower_1 = Conv2D(64, (3, 3), border_mode='same', activation='relu')(tower_1)
+    tower_2 = Conv2D(64, (1, 1), border_mode='same', activation='relu')(x)
+    tower_2 = Conv2D(64, (5, 5), border_mode='same', activation='relu')(tower_2)
     tower_3 = MaxPool2D((3, 3), strides=(1, 1), border_mode='same')(x)
-    tower_3 = Conv2D(64, 1, 1, border_mode='same', activation='relu')(tower_3)
+    tower_3 = Conv2D(64, (1, 1), border_mode='same', activation='relu')(tower_3)
     x = merge([tower_0, tower_1, tower_2, tower_3], mode='concat', concat_axis=3)
     x = Dropout(0.5)(x)
 
-    tower_0 = Conv2D(32, 1, 1, border_mode='same', activation='relu')(x)
-    tower_1 = Conv2D(32, 1, 1, border_mode='same', activation='relu')(x)
-    tower_1 = Conv2D(32, 3, 3, border_mode='same', activation='relu')(tower_1)
-    tower_2 = Conv2D(32, 1, 1, border_mode='same', activation='relu')(x)
-    tower_2 = Conv2D(32, 5, 5, border_mode='same', activation='relu')(tower_2)
+    tower_0 = Conv2D(32, (1, 1), border_mode='same', activation='relu')(x)
+    tower_1 = Conv2D(32, (1, 1), border_mode='same', activation='relu')(x)
+    tower_1 = Conv2D(32, (3, 3), border_mode='same', activation='relu')(tower_1)
+    tower_2 = Conv2D(32, (1, 1), border_mode='same', activation='relu')(x)
+    tower_2 = Conv2D(32, (5, 5), border_mode='same', activation='relu')(tower_2)
     tower_3 = MaxPool2D((3, 3), strides=(1, 1), border_mode='same')(x)
-    tower_3 = Conv2D(32, 1, 1, border_mode='same', activation='relu')(tower_3)
+    tower_3 = Conv2D(32, (1, 1), border_mode='same', activation='relu')(tower_3)
     x = merge([tower_0, tower_1, tower_2, tower_3], mode='concat', concat_axis=3)
     x = Dropout(0.5)(x)
 
@@ -114,39 +139,39 @@ def get_model_4(area):
     """First res network implementation"""
     x = input_image = Input(shape=(area, area, 14))
 
-    x = Conv2D(64, 1, 1, border_mode='same')(x)
+    x = Conv2D(filters=64, kernel_size=(1, 1), border_mode='same')(x)
 
-    y = Conv2D(64, 3, 1, border_mode='same')(x)
+    y = Conv2D(filters=64, kernel_size=(3, 1), border_mode='same')(x)
     y = Activation('relu')(y)
-    y = Conv2D(64, 1, 3, border_mode='same')(y)
+    y = Conv2D(filters=64, kernel_size=(1, 3), border_mode='same')(y)
     y = Activation('relu')(y)
-    y = Conv2D(64, 3, 1, border_mode='same')(y)
+    y = Conv2D(filters=64, kernel_size=(3, 1), border_mode='same')(y)
     y = Activation('relu')(y)
-    y = Conv2D(64, 1, 3, border_mode='same')(y)
+    y = Conv2D(filters=64, kernel_size=(1, 3), border_mode='same')(y)
     # this returns x + y.
     x = merge([x, y], mode='sum')
     x = Activation('relu')(x)
     x = MaxPool2D(pool_size=(2, 2))(x)
 
-    y = Conv2D(64, 3, 1, border_mode='same')(x)
+    y = Conv2D(filters=64, kernel_size=(3, 1), border_mode='same')(x)
     y = Activation('relu')(y)
-    y = Conv2D(64, 1, 3, border_mode='same')(y)
+    y = Conv2D(filters=64, kernel_size=(1, 3), border_mode='same')(y)
     y = Activation('relu')(y)
-    y = Conv2D(64, 3, 1, border_mode='same')(y)
+    y = Conv2D(filters=64, kernel_size=(3, 1), border_mode='same')(y)
     y = Activation('relu')(y)
-    y = Conv2D(64, 1, 3, border_mode='same')(y)
+    y = Conv2D(filters=64, kernel_size=(1, 3), border_mode='same')(y)
     # this returns x + y.
     x = merge([x, y], mode='sum')
     x = Activation('relu')(x)
-    x = Conv2D(32, 1, 1, border_mode='same')(x)
+    x = Conv2D(filters=32, kernel_size=(1, 1), border_mode='same')(x)
 
-    y = Conv2D(32, 3, 1, border_mode='same')(x)
+    y = Conv2D(filters=32, kernel_size=(3, 1), border_mode='same')(x)
     y = Activation('relu')(y)
-    y = Conv2D(32, 1, 3, border_mode='same')(y)
+    y = Conv2D(filters=32, kernel_size=(1, 3), border_mode='same')(y)
     y = Activation('relu')(y)
-    y = Conv2D(32, 3, 1, border_mode='same')(y)
+    y = Conv2D(filters=32, kernel_size=(3, 1), border_mode='same')(y)
     y = Activation('relu')(y)
-    y = Conv2D(32, 1, 3, border_mode='same')(y)
+    y = Conv2D(filters=32, kernel_size=(1, 3), border_mode='same')(y)
     # this returns x + y.
     x = merge([x, y], mode='sum')
     x = Activation('relu')(x)
@@ -165,7 +190,8 @@ model_pool = {
     "simple_conv"       : get_model_1,
     "medium_maxout_conv": get_model_2,
     "inception_net"     : get_model_3,
-    "resnet"            : get_model_4
+    "resnet"            : get_model_4,
+    "cifar"             : get_model_cifar
 }
 
 
