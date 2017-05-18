30cf3442dd9f67373623ddb8c85417be933cc36d
Rene Knaebel <reneknaebel90@gmail.com>
2017-05-18 11:28:22 +0200

    minor fixes
    Signed-off-by: Rene Knaebel <reneknaebel90@gmail.com>

--- test/test_h5dataset.py
+++ test/test_h5dataset.py
@@ -5,11 +5,11 @@ import h5dataset
 def test_create_h5dataset():
     path = "/tmp/data.h5"
     files = "data/"
-    h5dataset.make_small_h5dataset(path)
+    h5dataset.make_dataset(files, path)
     sat_images = dataset.load_sat_images(files)
     altitude, slope = dataset.load_static_data(files)
-    
-    gen = h5dataset.patch_generator_from_small_h5(path, sat_images, altitude, slope, 25, 256, 0.4)
+
+    gen = h5dataset.patch_generator_from_h5(path, sat_images, altitude, slope, 25, 256, 0.4)
     next(gen)
 
 
@@ -18,6 +18,6 @@ def test_load_existing_h5dataset():
     files = "data/"
     sat_images = dataset.load_sat_images(files)
     altitude, slope = dataset.load_static_data(files)
-    
-    gen = h5dataset.patch_generator_from_small_h5(path, sat_images, altitude, slope, 25, 256, 0.4)
+
+    gen = h5dataset.patch_generator_from_h5(path, sat_images, altitude, slope, 25, 256, 0.4)
     next(gen)
