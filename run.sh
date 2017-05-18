8842620cddb535f36e85f6c75f83176db9a51029
Rene Knaebel <reneknaebel90@gmail.com>
2017-05-18 10:50:27 +0200

    added some model runs in shell script
    Signed-off-by: Rene Knaebel <reneknaebel90@gmail.com>

--- run.sh
+++ run.sh
@@ -1,1 +1,20 @@
-python3 main.py --mode train --data /home/ahmed/data/ --model models/model_x1.h5 --type simple_conv --batch 1024 --epochs 10
+# evaluation
+python3 main.py --mode train --data /home/ahmed/data/ --type simple_conv --model models/model_x1.h5 --h5data /tmp/data.h5 \
+                --area 25 \
+                --samples 250000 --batch 2048 --p 0.4 --epochs 100
+
+python3 main.py --mode train --data /home/ahmed/data/ --type medium_maxout_conv --model models/model_x2.h5 --h5data /tmp/data.h5 \
+                --area 25 \
+                --samples 250000 --batch 2048 --p 0.4 --epochs 100
+
+python3 main.py --mode train --data /home/ahmed/data/ --type inception_net --model models/model_x3.h5 --h5data /tmp/data.h5 \
+                --area 25 \
+                --samples 250000 --batch 2048 --p 0.4 --epochs 100
+
+python3 main.py --mode train --data /home/ahmed/data/ --type resnet --model models/model_x4.h5 --h5data /tmp/data.h5 \
+                --area 25 \
+                --samples 250000 --batch 2048 --p 0.4 --epochs 100
+
+python3 main.py --mode train --data /home/ahmed/data/ --type cifar --model models/model_x5.h5 --h5data /tmp/data.h5 \
+                --area 25 \
+                --samples 250000 --batch 2048 --p 0.4 --epochs 100
